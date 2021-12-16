1806a3ef8e05a1d05c8461fce73e4b2bf73c001d
Estelle Weisgerber <akumaony@gmail.com>
2021-12-16 14:07:48 +0100

    created scss

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,7 @@ Encore
      */
     .addEntry('app', './assets/app.js')
     .addStyleEntry('contact', './assets/styles/contact.scss')
+    .addStyleEntry('home', './assets/styles/home.scss')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
