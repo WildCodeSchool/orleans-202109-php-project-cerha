5ded2fc68dc52a1715a3d4a651760d97a9121910
Nathan Chapelle <nathanchapelle76@gmail.com>
2021-12-17 02:05:20 +0100

    regrouped the 3 migrations version into one for the employee table

--- migrations/Version20211217010327.php
+++ migrations/Version20211217010327.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211215091335 extends AbstractMigration
+final class Version20211217010327 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,7 +20,7 @@ final class Version20211215091335 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE employee (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, picture VARCHAR(255) NOT NULL, role VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE employee (id INT AUTO_INCREMENT NOT NULL, picture VARCHAR(255) NOT NULL, role VARCHAR(255) NOT NULL, civility VARCHAR(10) NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
