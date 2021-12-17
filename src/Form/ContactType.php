3fee0a37393c245463e5c1fc0136a9e89d97322b
Bsir <roulgamous@gmail.com>
2021-12-17 11:04:16 +0100

    form contact

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -5,8 +5,9 @@ namespace App\Form;
 use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -16,12 +17,17 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('company', ChoiceType::class, ['choices' => ['Entreprise' => 'Entreprise',
-             'Candidat' => 'Candidat'], 'expanded' => true, 'multiple' => false])
+            ->add('company', ChoiceType::class, [
+                'choices' => [
+                    'Entreprise' => 'Entreprise',
+                    'Candidat' => 'Candidat'
+                ],
+                'expanded' => true, 'multiple' => false
+            ])
             ->add('firstName', TextType::class, array('label' => 'Nom'))
             ->add('lastName', TextType::class, array('label' => 'Prénom'))
-            ->add('phone', TextType::class, array('label' => 'Téléphone'))
-            ->add('email', TextType::class, array('label' => 'E-mail'))
+            ->add('phone', TelType::class, array('label' => 'Téléphone'))
+            ->add('email', EmailType::class, array('label' => 'E-mail'))
             ->add('message', TextareaType::class, array('label' => 'Message'));
     }
 
