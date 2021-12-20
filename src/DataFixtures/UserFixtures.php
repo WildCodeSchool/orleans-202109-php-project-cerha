36753ad9354b503a60118f948d2863237190b6c5
Chapelle Nathan <nathanchapelle76@gmail.com>
2021-12-20 18:17:16 +0100

    Login form

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -6,10 +6,16 @@ use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
-    public const USER_NUMBER = 100;
+    private UserPasswordHasherInterface $passwordHasher;
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+    public const USER_NUMBER = 10;
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
@@ -19,11 +25,21 @@ class UserFixtures extends Fixture
             $user->setFirstname($faker->firstName());
             $user->setEmail($faker->email());
             $user->setPhoneNumber((int)$faker->serviceNumber());
-            $user->setPassword($faker->password());
+            $user->setPassword($this->passwordHasher->hashPassword($user, $faker->password()));
             $user->setGender('M');
             $user->setRoles(['ROLE_USER']);
             $manager->persist($user);
         }
+
+        $user = new User();
+        $user->setLastname('John');
+        $user->setFirstname('Doe');
+        $user->setEmail('john@doe.com');
+        $user->setPhoneNumber('0601010101');
+        $user->setPassword($this->passwordHasher->hashPassword($user, 'admin'));
+        $user->setGender('M');
+        $user->setRoles(['ROLE_ADMIN']);
+        $manager->persist($user);
 
         $manager->flush();
     }
