7f640abeefda37633a5783bd85e5b2b50ad8ca46
Chapelle Nathan <nathanchapelle76@gmail.com>
2021-12-20 19:21:36 +0100

    can logout

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,8 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Exception;
 
-class LoginController extends AbstractController
+class SecurityController extends AbstractController
 {
     /**
      * @Route("/login", name="login")
@@ -22,5 +23,13 @@ class LoginController extends AbstractController
             'last_username' => $lastUsername,
             'error' => $error,
         ]);
+    }
+
+    /**
+     * @Route("/logout", name="logout", methods={"GET"})
+     */
+    public function logout(): void
+    {
+        throw new Exception('Don\'t forget to activate logout in security.yaml');
     }
 }
