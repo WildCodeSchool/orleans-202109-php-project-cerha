8871f8eb7e1346bb89470aff4b283fb28eaebe8a
Estelle Weisgerber <akumaony@gmail.com>
2022-01-03 11:42:31 +0100

    fixed error grump test

--- src/Controller/CandidatController.php
+++ src/Controller/CandidatController.php
@@ -7,6 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
 
 /**
  * @Route("/candidat", name="candidat_")
@@ -23,7 +24,10 @@ class CandidatController extends AbstractController
         HobbyRepository $hobbyRepository
     ): Response {
         $hobbies = $hobbyRepository->findAll();
-        $candidat = $this->getUser()->getCandidat();
+        /** @var User */
+        $user = $this->getUser();
+        $candidat = $user->getCandidat();
+
         return $this->render(
             'candidat/index.html.twig',
             ['candidat' => $candidat, 'hobbies' => $hobbies]
