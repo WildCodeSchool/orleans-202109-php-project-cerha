5489c5e418be13c56a567699bee2a207f0b25486
Estelle Weisgerber <akumaony@gmail.com>
2022-01-03 16:51:08 +0100

    CandidatLanguageFixtures created

---
+++ src/DataFixtures/CandidatLanguageFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\CandidatLanguage;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CandidatLanguageFixtures extends Fixture implements DependentFixtureInterface
+{
+    public const LANGUAGES = ['fr_FR', 'fr_BE', 'en_EN'];
+    public function load(ObjectManager $manager): void
+    {
+        foreach (self::LANGUAGES as $language) {
+            $newLanguage = new CandidatLanguage();
+            $newLanguage->setName($language);
+            $newLanguage->setCandidat($this->getReference('candidat_john'));
+            $manager->persist($newLanguage);
+        }
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            CandidatFixtures::class
+        ];
+    }
+}
