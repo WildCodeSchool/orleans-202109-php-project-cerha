0b3314abce81bddaa88d120bba5233cd7684f6f6
Thibault <thibaultfoure@gmail.com>
2022-01-05 17:19:41 +0100

    ok form is working omg

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Company;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\CompanyContactType;
+use Doctrine\ORM\EntityManagerInterface;
 
 /**
  * @Route("/entreprise", name="company_")
@@ -25,5 +28,28 @@ class CompanyController extends AbstractController
             'company/index.html.twig',
             ['company' => $company]
         );
+    }
+
+    /**
+     * @Route("/profil/{id<^[0-9]+$>}/modifier", name="edit", methods={"GET", "POST"})
+     */
+    public function editCompanyContact(
+        Request $request,
+        Company $company,
+        EntityManagerInterface $entityManager
+    ): Response {
+        $form = $this->createForm(CompanyContactType::class, $company);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('company_show', ['id' => $company->getId()], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('company/editCompanyContact.html.twig', [
+            'company' => $company,
+            'form' => $form,
+        ]);
     }
 }
---
+++ src/Form/CompanyContactType.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Company;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CompanyContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('contactRole');
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Company::class,
+        ]);
+    }
+}
