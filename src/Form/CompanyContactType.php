49346b6dfa08db7121153ca9903d258c0f1bc36d
Thibault <thibaultfoure@gmail.com>
2022-01-06 17:44:06 +0100

    added all fileds to modify in contact company

--- src/Form/CompanyContactType.php
+++ src/Form/CompanyContactType.php
@@ -6,13 +6,16 @@ use App\Entity\Company;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use App\Form\UserCompanyType;
 
 class CompanyContactType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('contactRole');
+            ->add('user', UserCompanyType::class, ['label' => false])
+            ->add('contactRole', TextType::class, ['label' => 'Intitulé de poste du contact']);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
---
+++ src/Form/UserCompanyType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use PhpParser\Node\Stmt\Label;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class UserCompanyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('gender', ChoiceType::class, ['choices' => ['M.' => 'M.', 'Mme' => 'Mme'], 'label' => 'M. ou Mme'])
+            ->add('lastname', TextType::class, ['label' => 'Nom'])
+            ->add('firstname', TextType::class, ['label' => 'Prénom'])
+            ->add('phoneNumber', TextType::class, ['label' => 'Numéro de téléphone']);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
