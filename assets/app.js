d6ad56fc68b4a770f2bc899b921d56e1aae7d478
Bsir <roulgamous@gmail.com>
2022-01-10 17:26:17 +0100

    fix-style-candidat

--- assets/app.js
+++ assets/app.js
@@ -8,6 +8,9 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
+// Import boostrap icon
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 // start the Stimulus application
 import './bootstrap';
 
