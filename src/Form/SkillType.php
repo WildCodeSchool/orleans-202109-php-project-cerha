84816a20b8f4de87d309af02cbc33866fad5e060
Bsir <roulgamous@gmail.com>
2022-01-10 17:22:33 +0100

    US13-S02-edit-skill

--- src/Form/SkillType.php
+++ src/Form/SkillType.php
@@ -2,18 +2,22 @@
 
 namespace App\Form;
 
+use App\Entity\Sector;
 use App\Entity\Skill;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class SkillType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class, ['label' => 'compétences']);
+            ->add('sector', null, ['choice_label' => 'name'])
+            ->add('name', TextareaType::class, ['label' => 'compétences']);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
