d52ae940306eec4bfab9d37dde52c0255e21d9d4
Estelle Weisgerber <akumaony@gmail.com>
2022-01-10 21:48:28 +0100

    add forms for hobby

--- src/Form/HobbiesType.php
+++ src/Form/HobbiesType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Candidat;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
--- src/Form/HobbyType.php
+++ src/Form/HobbyType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Hobby;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
