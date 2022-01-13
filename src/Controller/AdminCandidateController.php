babec8c7ce732625a89860335a1838d7413a2284
Estelle Weisgerber <akumaony@gmail.com>
2022-01-13 23:47:10 +0100

    -e missed

--- src/Controller/AdminCandidateController.php
+++ src/Controller/AdminCandidateController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/admin/candidate")
+ * @Route("/admin/candidat")
  */
 class AdminCandidateController extends AbstractController
 {
