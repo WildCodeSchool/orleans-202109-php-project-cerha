48e6ac34c6631d2fb608890c39829b24a48a4190
Estelle Weisgerber <akumaony@gmail.com>
2022-01-17 21:20:02 +0100

    add use for isGranted

--- src/Controller/AdminEmployeeController.php
+++ src/Controller/AdminEmployeeController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
  * @Route("/admin/membres")
