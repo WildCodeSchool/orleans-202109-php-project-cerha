c044e77365da005e3fdc3bcdefe63509544ae548
Thibault <thibaultfoure@gmail.com>
2022-01-18 13:49:30 +0100

    commit avant fusion

--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -5,14 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ServiceRepository;
 
 class ServiceController extends AbstractController
 {
     /**
      * @Route("/services", name="services")
      */
-    public function index(): Response
+    public function index(ServiceRepository $serviceRepository): Response
     {
-        return $this->render('services/index.html.twig');
+        return $this->render('services/index.html.twig', ['services' => $serviceRepository->findAll()]);
     }
 }
