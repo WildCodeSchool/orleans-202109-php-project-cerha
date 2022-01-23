4eee5f7d5073ef337ff0714416e0522df0d7ca0f
Estelle Weisgerber <akumaony@gmail.com>
2022-01-23 13:37:43 +0100

    add style and secure route to delete member

--- src/Controller/AdminEmployeeController.php
+++ src/Controller/AdminEmployeeController.php
@@ -82,6 +82,7 @@ class AdminEmployeeController extends AbstractController
 
     /**
      * @Route("/{id}", name="admin_employee_delete", methods={"POST"})
+     * @IsGranted("ROLE_ADMIN")
      */
     public function delete(Request $request, Employee $employee, EntityManagerInterface $entityManager): Response
     {
